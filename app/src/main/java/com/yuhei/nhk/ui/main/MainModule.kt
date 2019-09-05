9f4a27a9aca2288c92f34fe039db125ad6501c01
YuheiKawakami <yuhei8902@gmail.com>
2019-09-05 15:09:04 +0900

    Fix dagger

---
+++ app/src/main/java/com/yuhei/nhk/ui/main/MainModule.kt
@@ -0,0 +1,20 @@
+package com.yuhei.nhk.ui.main
+
+import androidx.lifecycle.ViewModel
+import com.yuhei.nhk.di.ViewModelKey
+import dagger.Binds
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+import dagger.multibindings.IntoMap
+
+@Module
+abstract class MainModule {
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(MainViewModel::class)
+    abstract fun bindMainViewModel(viewModel: MainViewModel): ViewModel
+
+    @ContributesAndroidInjector
+    abstract fun contributeMainFragment(): MainFragment
+}
--- app/src/main/java/com/yuhei/nhk/ui/main/MainViewModel.kt
+++ app/src/main/java/com/yuhei/nhk/ui/main/MainViewModel.kt
@@ -1,4 +1,4 @@
-package com.yuhei.nhk.ui
+package com.yuhei.nhk.ui.main
 
 import androidx.lifecycle.ViewModel
 import javax.inject.Inject

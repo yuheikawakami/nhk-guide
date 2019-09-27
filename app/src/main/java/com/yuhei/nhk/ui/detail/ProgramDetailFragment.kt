f6d5e8ae6d9073c138d422108653926a0186eb9a
YuheiKawakami <yuhei8902@gmail.com>
2019-09-27 17:45:24 +0900

    Add temporary layout of program list and program detail

--- app/src/main/java/com/yuhei/nhk/ui/detail/ProgramDetailFragment.kt
+++ app/src/main/java/com/yuhei/nhk/ui/detail/ProgramDetailFragment.kt
@@ -1,4 +1,4 @@
-package com.yuhei.nhk.ui.program
+package com.yuhei.nhk.ui.detail
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,8 +7,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.yuhei.nhk.R
 
-class ProgramListFragment : Fragment() {
+class ProgramDetailFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_program_list, container, false)
+        return inflater.inflate(R.layout.fragment_program_detail, container, false)
     }
 }
